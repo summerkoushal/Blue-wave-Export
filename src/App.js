30258006455e4d14d22a98b509b47b68d41b3624
summekoushal <2020bcs003@sggs.ac.in>
2025-06-21 00:28:57 +0530

    completed upto contact page

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Products from "./components/Products";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+      {/* <Home /> */}
+      {/* <AboutUs /> */}
+      {/* <Products /> */}
+      {/* <ContactUs /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AboutUs.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <>
+      <div className="aboutus-container1">
+        <img className="aboutus-img" src="/pulses.jpg" />
+        <div className="aboutus-text-container">
+          <h5 className="text-secondary aboutus-text-heading">ABOUT</h5>
+          <h2 className="aboutus-text-subheading">
+            <b>Blue wave Exports</b>
+          </h2>
+          <i>
+            alias omnis, deserunt cupiditate atque eligendi officia perferendis
+            laboriosam voluptatem, libero reprehenderit quis vitae iusto,
+            aliquam nisi odit vel natus quae dolore! Praesentium, enim odit quod
+            vero nam consequatur reprehenderit aliquam repellat tenetur magnam
+            quia a atque. Facilis quam asperiores repellendus facere nulla.
+            Necessitatibus, odio rerum? Minima facere tenetur, adipisci vel
+            autem molestiae ex iste maiores. Praesentium, illum quae quia,
+            Voluptates tempora veritatis, nobis nemo dolorum molestiae quidem
+            aspernatur odit labore nihil! Hic totam impedit, alias nemo cumque
+            odit minus rerum quisquam tempore corrupti ut labore, voluptate
+            corporis incidunt dolor quas tenetur eum dolorem possimus nostrum.
+            Labore minus perferendis dolor, omnis quo ipsum harum aspernatur eos
+            similique nemo eaque adipisci, velit eum excepturi dolorum dolores
+            obcaecati voluptatum voluptates pariatur ad maxime tenetur. Quisquam
+            aliquam natus eligendi in similique atque dolorum eius. Sequi
+            maiores, sit commodi facere nisi doloremque quibusdam voluptate,
+            illo, ut eos recusandae rerum. Magni neque deserunt dolor autem,
+            architecto officiis harum maiores quas eos, similique ab vitae quae.
+            Vitae distinctio nemo minima voluptas a nisi sed quas nostrum quasi
+            odio, laboriosam sint unde nihil repellat corporis debitis eius
+            veritatis dolorum? Laboriosam pariatur, quas consequatur ex
+            quibusdam expedita harum quidem deleniti magnam amet enim, odit
+            illum cupiditate dolorem sed dicta. Vitae distinctio nemo minima
+            voluptas a nisi sed quas nostrum quasi odio, laboriosam sint unde
+            nihil repellat corporis debitis eius veritatis dolorum?
+          </i>
+        </div>
+      </div>
+    </>
+  );
+}
