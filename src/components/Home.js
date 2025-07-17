fe42b86db9a6888705f7750354565d15fd07299e
summekoushal <2020bcs003@sggs.ac.in>
2025-07-17 15:38:31 +0530

    all design done except product page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import HomeAbout from './HomeAbout';
-import HomeExports from './HomeExports';
-import HomeProducts from './HomeProducts';
+import React from "react";
+import HomeAbout from "./HomeAbout";
+import HomeExports from "./HomeExports";
+import HomeProducts from "./HomeProducts";
 
 export default function Home() {
   return (
     <>
-      <div className="home-container1">
-        <img style={{width: '100%'}} src="home.jpg" alt="" />
-        <p style={{fontSize: '70px', position: 'absolute', top:'400px', left: '23vw', color: 'white', fontWeight: '600'}} >- Agriculture Products -</p>
-        <button className='btn btn-primary' style={{position: 'absolute', top: '520px', left: '43vw', fontSize: '20px'}} >Our Products</button>
+      <div className="home-main-container">
+        <img className="home-main-img" src="home.jpg" alt="Home"/>
+        <div className="home-main-text-container">
+          <p className="home-main-text">- Agriculture Products -</p>
+          <button className="btn btn-primary home-main-btn">Our Products</button>
+        </div>
       </div>
       <HomeAbout />
       <HomeExports />
